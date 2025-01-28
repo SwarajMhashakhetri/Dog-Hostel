01c5c10472698e2e6275e8fa0671c25c327f95c5
SwarajMhashakhetri <swarajmhashakhetri20@gmail.com>
2025-01-29 00:16:54 +0530

    Added nextAuth

---
+++ app/lib/auth.ts
@@ -0,0 +1,81 @@
+import prisma from "@/db"; 
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        name: { label: "Name", type: "text", placeholder: "Your Name" },
+        email: { label: "Email", type: "text", placeholder: "example@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials : any) {
+        const { email, password } = credentials;
+
+        if (!email || !password) {
+          throw new Error("Email and password are required");
+        }
+
+        const existingUser = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (existingUser) {
+          const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+          if (isPasswordValid) {
+            return {
+              id: existingUser.id.toString(),
+              name: existingUser.name,
+              email: existingUser.email,
+              role: existingUser.role,
+            };
+          } else {
+            throw new Error("Invalid password");
+          }
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try {
+          const newUser = await prisma.user.create({
+            data: {
+              name: credentials.name,
+              email,
+              password: hashedPassword,
+              role: "OWNER",
+            },
+          });
+
+          return {
+            id: newUser.id.toString(),
+            name: newUser.name,
+            email: newUser.email,
+            role: newUser.role,
+          };
+        } catch (error) {
+          console.error("Error creating user:", error);
+          throw new Error("Failed to create user");
+        }
+      },
+    }),
+  ],
+  secret: process.env.JWT_SECRET || "secret",
+  callbacks: {
+    async jwt({ token, user }:any) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role; 
+      }
+      return token;
+    },
+    async session({ session, token } :any) {
+      session.user = {
+        id: token.id,
+        role: token.role, 
+      };
+      return session;
+    },
+  },
+};
---
+++ prisma/migrations/20250128160510_init/migration.sql
@@ -0,0 +1,46 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('OWNER', 'LENDER');
+
+-- CreateEnum
+CREATE TYPE "BookingStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'OWNER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pet" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "breed" TEXT NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+    "specialRequirements" TEXT,
+
+    CONSTRAINT "Pet_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" SERIAL NOT NULL,
+    "petId" INTEGER NOT NULL,
+    "lenderId" INTEGER NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "status" "BookingStatus" NOT NULL DEFAULT 'PENDING',
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Pet" ADD CONSTRAINT "Pet_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
